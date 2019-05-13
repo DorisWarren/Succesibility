138a60dbff87ff73d1108995a52ff26899dd33b8
Gary Bermudez and Victoria Martinez <victoria@>
2019-05-13 15:05:02 -0700

    create header content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from './components/carousel/Carousel';
+import Header from './components/header/Header';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Carousel />
     </div>
   );
---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './../../assets/Logo.png';
+
+function Logo(){
+  return(
+    <div>
+
+      <img src={logo} alt="Portland Farmers Market Logo"></img>
+    </div>
+
+  )
+
+}
+export default Logo;
