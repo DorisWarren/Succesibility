56aeea0874e862f223d39394768f91269cd7877c
Chris Breaux <christopherjbreaux@gmail.com>
2019-05-13 16:01:11 -0700

    add react-slick

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,55 +1,45 @@
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
+import Slider from 'react-slick';
+
+
+
 
 class Carousel extends React.Component {
 
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
 
-    const images = [
-      {
-        original: require('../../assets/Asparagus_PSU_2018.jpg'),
-        originalAlt: 'A bunch of asparagus',
-        originalTitle: 'Check Out Out Latest News',
-        description: 'Large bunches of asparagus held together with blue rubber bands fill the page'
-
-      },
-      {
-        original: require('../../assets/pfmkids_99-credit-midres.jpg'),
-        originalAlt: 'Children making food together',
-        originalTitle: 'Kids Cook at the Market',
-        description: 'Three children are learning to cook at a picnic table outside. They are being assisted by two women.'
-
-      },
-      {
-        original: require('../../assets/RKH_1030-resized-cropped.jpg'),
-        orginalAlt: 'Bouquets of pink and yellow flowers',
-        originalTitle: 'PSU Farmers Market',
-        description: 'A closeup image of three bouquets of pink and yellow flowers.'
-
-      },
-      {
-        original: require('../../assets/RKH_1887-resized.jpg'),
-        originalAlt: 'Bunches of Swiss Chard',
-        originalTitle: 'Shemanski Park Farmers Market',
-        description: 'Bunches of red-stalked swiss chard are displayed with their leaves trimmed.'
-
-      },
-      {
-        original: require('../../assets/RKH_1933-resized.jpg'),
-        oringalAlt: 'Bunches of organic Welsh onions',
-        originalTitle: 'King Farmers Market',
-        description: 'A basket containing several bunches of long Welsh onions with yellow organic labels and thin purple bulbs.'
-
-      },
-
-    ]
 
     return (
       <div>
-      <ImageGallery
-        items={images}
-        showThumbnails={false}
-        showPlayButton={false} />
+      <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <img src={require('./../..assets/Asparagus_PSU_2018.jpg')}
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+
       </div>
     );
   }
