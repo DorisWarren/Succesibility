07a52f50e31ded63414e32a43dfe1d78ae8cdb36
Chris Breaux and Maly Phongsavanh <chris@>
2019-05-14 11:19:43 -0700

    import fontawesome icons and display each icon imported

--- src/components/footer/Contacts.jsx
+++ src/components/footer/Contacts.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBicycle, faClock, faPhone, faInbox, faCog } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBicycle, faClock, faPhone, faInbox, faCog);
 
 function Contacts() {
   return (
     <div>
       <h1>CONTACT OUR OFFICE</h1>
+        <FontAwesomeIcon icon="bicycle"/>
+        <FontAwesomeIcon icon="clock"/>
+          <FontAwesomeIcon icon="phone"/>
+            <FontAwesomeIcon icon="inbox"/>
+              <FontAwesomeIcon icon="cog"/>
     </div>
   );
 }
