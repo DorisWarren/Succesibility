22e23c44b7f53a7c0a492a2e02f6530a9bb7af84
Chris Breaux and Maly Phongsavanh <chris@>
2019-05-14 09:20:17 -0700

    add embeded PortlandFarmers twitter feed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Carousel from './components/carousel/Carousel';
 import Header from './components/header/Header';
 import Sponsors from './components/sponsors/Sponsors';
+import Footer from './components/footer/Footer';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Carousel />
       <Sponsors />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <div>
+    <a class="twitter-timeline" data-width="400" data-height="800" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Tweets by portlandfarmers</a>
+    </div>
+  );
+}
+
+export default Footer;
