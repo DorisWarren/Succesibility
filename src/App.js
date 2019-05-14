bcb08a2c51e377b62347d0aee16df3c5e9fe8e70
Doris Warren <doriswarren11@gmail.com>
2019-05-14 08:30:47 -0700

    add images to sponsors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Carousel from './components/carousel/Carousel';
-
+import Sponsors from './components/sponsors/Sponsors';
 
 function App() {
   return (
     <div>
       <Carousel />
+      <Sponsors />
     </div>
   );
 }
---
+++ src/components/sponsors/Sponsors.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import amica from '../../assets/amica.png';
+import daveskillerbread from '../../assets/daveskillerbread.png';
+import pge from '../../assets/pge.jpg';
+
+function Sponsors() {
+  const styles={
+  }
+return (
+    <div>
+      <div styles={styles}>
+        <style jsx>{`
+          img {
+            width: 20%;
+            margin-right: 5%;
+        `}</style>
+        <img src={amica} alt='amica'/>
+        <img src={daveskillerbread} alt='daveskillerbread'/>
+        <img src={pge} alt='pge'/>
+      </div>
+    </div>
+  );
+}
+
+export default Sponsors;
