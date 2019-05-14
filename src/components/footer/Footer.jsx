11e67941993171150025b838bb6907cb3da0e771
Chris Breaux and Maly Phongsavanh <maly@>
2019-05-14 09:42:13 -0700

    install Iframe and render instagram feed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Iframe from 'react-iframe'
 
 function Footer() {
   return(
     <div>
+      <Iframe src="//lightwidget.com/widgets/10897160832f5a1d8758fdc13cc5e178.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></Iframe>
     <a class="twitter-timeline" data-width="400" data-height="800" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Tweets by portlandfarmers</a>
     </div>
   );
