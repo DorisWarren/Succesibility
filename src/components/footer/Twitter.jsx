5c909e0bb494d912cef5247cb8a9b82c94d6b82f
Doris Warren and Maly Phongsavanh <maly@>
2019-05-15 13:18:35 -0700

    revert embedded twitter feed properties

--- src/components/footer/Twitter.jsx
+++ src/components/footer/Twitter.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 function Twitter() {
   return(
     <div>
+      <style jsx>{`
+          a .customisable-hightlight {
+            color: white !important;
+          }`}</style>
       <h5>FOLLOW US ON TWITTER</h5>
-      <a className="twitter-timeline" data-height="350" data-theme="light" data-link-color="#ffffff" data-chrome="transparent" data-tweet-limit="1" data-aria-polite="assertive" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Recent Tweet by portlandfarmers</a>
+      <a className="twitter-timeline" data-height="350" data-theme="dark" data-link-color="#ffffff" data-chrome="transparent" data-tweet-limit="1" data-aria-polite="assertive" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Recent Tweet by portlandfarmers</a>
     </div>
   );
 }
