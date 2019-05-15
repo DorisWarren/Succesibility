5d13d5725873e3f64e18309140ba4fc30cec0180
Doris Warren and Maly Phongsavanh <doris@>
2019-05-15 11:21:19 -0700

    color correct elements in multiple components

--- src/components/footer/Twitter.jsx
+++ src/components/footer/Twitter.jsx
@@ -4,7 +4,7 @@ function Twitter() {
   return(
     <div>
       <h5>FOLLOW US ON TWITTER</h5>
-      <a className="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Tweets by portlandfarmers</a>
+<a class="twitter-timeline" data-height="350" data-theme="dark" data-link-color="#F5F8FA" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Tweets by portlandfarmers</a>
     </div>
   );
 }
