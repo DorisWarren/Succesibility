1b44cfb02fde34d99290176b36264695c9bbf044
Doris Warren and Maly Phongsavanh <doris@>
2019-05-15 12:13:17 -0700

    set embedded twitter feed background to transparent

--- src/components/footer/Twitter.jsx
+++ src/components/footer/Twitter.jsx
@@ -4,7 +4,7 @@ function Twitter() {
   return(
     <div>
       <h5>FOLLOW US ON TWITTER</h5>
-<a class="twitter-timeline" data-height="350" data-theme="dark" data-link-color="#F5F8FA" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Tweets by portlandfarmers</a>
+      <a className="twitter-timeline" data-height="350" data-theme="dark" data-link-color="#F5F8FA" data-chrome="transparent" href="https://twitter.com/portlandfarmers?ref_src=twsrc%5Etfw">Tweets by portlandfarmers</a>
     </div>
   );
 }
