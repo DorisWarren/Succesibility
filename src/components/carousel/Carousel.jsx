cbea8c431071ae64b17c4c4c8edaf4b0757fc14e
Chris Breaux and Victoria Martinez <victoria@>
2019-05-15 09:51:07 -0700

    add tabindex, change carousel display

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -28,31 +28,31 @@ class Carousel extends React.Component {
         <Slider {...settings}>
           <div id="image1">
             <div className="overlay">
-              <h1>CHECK OUT OUR LATEST NEWS</h1>
+              <h1 tabindex="0">CHECK OUT OUR LATEST NEWS</h1>
             </div>
           </div>
           <div id="image2">
             <div className="overlay">
-              <h1>KING FARMERS MARKET</h1>
-              <h3>Open Sundays 10am to 2pm</h3>
+              <h1 tabindex="0">KING FARMERS MARKET</h1>
+              <h3 tabindex="0">Open Sundays 10am to 2pm</h3>
             </div>
           </div>
           <div id="image3">
             <div className="overlay">
-              <h1>SHEMANSKI PARK FARMERS MARKET</h1>
-              <h3>Open Wednesdays 10am to 2pm</h3>
+              <h1 tabindex="0">SHEMANSKI PARK FARMERS MARKET</h1>
+              <h3 tabindex="0">Open Wednesdays 10am to 2pm</h3>
             </div>
           </div>
           <div id="image4">
             <div className="overlay">
-              <h1>PSU FARMERS MARKET</h1>
-              <h3>Open Saturdays 8:30am to 2pm</h3>
+              <h1 tabindex="0">PSU FARMERS MARKET</h1>
+              <h3 tabindex="0">Open Saturdays 8:30am to 2pm</h3>
             </div>
           </div>
           <div id="image5">
             <div className="overlay">
-              <h1>KIDS COOK AT THE MARKET</h1>
-              <h3>Registration now open!</h3>
+              <h1 tabindex="0">KIDS COOK AT THE MARKET</h1>
+              <h3 tabindex="0">Registration now open!</h3>
             </div>
           </div>
         </Slider>
